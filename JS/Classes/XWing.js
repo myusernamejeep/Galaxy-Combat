2dba53f6d8e73a8171434f513b22bc343af037e0
Jake Champion <jakechampion@Jakes-MacBook-Air.local>
2013-03-08 17:19:55 +0000

    Fixed XWing speed and rotation

--- JS/Classes/XWing.js
+++ JS/Classes/XWing.js
@@ -29,8 +29,8 @@
         this.stage = stage;
         this.radius = Math.sqrt((this.height / 2)*(this.height / 2) + (this.width / 2) * (this.width / 2));
         this._health = 100;
-        this._speed = 5;
-        this._rotationSpeed = 7;
+        this._speed = 20;
+        this._rotationSpeed = 3;
         this.accX = 0;
         this.accY = 0;
         this._alive = true;
@@ -76,16 +76,16 @@
 
         XWing.prototype.moveForward = function() {
             var radians = this.rotation * (Math.PI / 180.0);
-            this.accX = Math.cos(radians) * this._speed;
-            this.accY = Math.sin(radians) * this._speed;
+            this.accX = Math.sin(radians) * this._speed;
+            this.accY = Math.cos(radians) * this._speed;
 
             // Update the horizontal position (x)
-            this.x += (this.accY) / 2;
+            this.x += (this.accX) / 2;
 
             // Update the vertical position (y)
             // Subtracted because coordinate system starts in upper right
             // and has positive y going downwards.
-            this.y -= (this.accX) / 2;
+            this.y -= (this.accY) / 2;
         };
 //        this.rotation = (Math.floor(Math.random() * 360));
 
