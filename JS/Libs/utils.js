5d91b475f2e805174b1b033529e9364bbbfee125
Jake Champion <jakechampion@Jakes-MacBook-Air.local>
2013-03-09 00:17:12 +0000

    utils added to help remove repeated functions

---
+++ JS/Libs/utils.js
@@ -0,0 +1,28 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jakechampion
+ * Date: 08/03/2013
+ * Time: 17:13
+ * To change this template use File | Settings | File Templates.
+ */
+function angleTo(a,b){
+    "use strict";
+    var pi = Math.PI;
+    return fixAngle((a.y>b.y?pi:0)+((a.y==b.y)?(b.x>a.x?pi/2:pi*3/2):Math.atan((a.x-b.x)/(a.y-b.y))));
+}
+
+function fixAngle(a){
+    "use strict";
+    var pi2 = Math.PI*2;
+    while(a < 0) {
+        a +=pi2;
+    }
+    while(a > pi2) {
+        a-=pi2;
+    }
+    return a;
+}
+function now(){
+    "use strict";
+    return (new window.Date()).getTime();
+}
